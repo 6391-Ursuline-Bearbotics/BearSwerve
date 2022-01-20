6ec14294105456243bf43b377cb39c6598646177
jasondaming <jasondaming@gmail.com>
2022-01-19 21:09:49 -0600

    additional options for setting module states

--- src/main/java/frc/swervelib/SwerveConstants.java
+++ src/main/java/frc/swervelib/SwerveConstants.java
@@ -1,11 +1,14 @@
 package frc.swervelib;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 
 public class SwerveConstants {
     public static double MAX_FWD_REV_SPEED_MPS;
+    public static double MAX_STRAFE_SPEED_MPS;
+    public static double MAX_ROTATE_SPEED_RAD_PER_SEC;
     public static double MAX_VOLTAGE;
     public static Pose2d DFLT_START_POSE;
     
@@ -19,4 +22,7 @@ public class SwerveConstants {
     public static double MASS_kg;
     public static double MOI_KGM2;
     public static SwerveDriveKinematics KINEMATICS;
+
+    public static PIDController XPIDCONTROLLER = new PIDController(TRAJECTORYXkP, 0, 0);
+    public static PIDController YPIDCONTROLLER = new PIDController(TRAJECTORYYkP, 0, 0);
 }
